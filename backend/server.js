3712ba9f020fdee377a0ba4e90c5ceb76abc1e33
Will Pringle <willkantorpringle+1@gmail.com>
2022-01-15 11:51:45 -0500

    Add endpoint for list

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 #!/usr/bin/env node
 
 const express = require('express');
-const path = require("path");
+const path = require('path');
+
+const inventory = require('./inventory');
 
 const app = express();
 
 // serve html
-app.get("/", express.static(path.join(__dirname, "../public")));
+app.get('/', express.static(path.join(__dirname, '../public')));
+
+app.get('/list', (req, res) => {
+  inventory.listItems()
+    .then(items => {
+      res.send(items);
+    })
+    .catch(error => {
+      res.status(502).send(error.message);
+    });
+});
+
 
 // run server process
 const server = app.listen(process.env.PORT || 8000, () => {
