6321301f1f6c4fe102bace8396669c671d88aae7
Will Pringle <willkantorpringle+1@gmail.com>
2022-01-15 12:30:09 -0500

    Add endpoints for edit and delete

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ const app = express();
 // serve html
 app.get('/', express.static(path.join(__dirname, '../public')));
 
-app.get('/create', (req, res) => {
-
+app.post('/create', (req, res) => {
+  // temporary hardcoded title for testing
   const title = 'test';
 
   inventory.createItem(title)
@@ -20,6 +20,31 @@ app.get('/create', (req, res) => {
     })
     .catch(error => {
       res.status(400).send(error.message);
+    });
+});
+
+app.post('/edit', (req, res) => {
+  // temporary hardcoded for testing
+  const id = 0;
+  const edits = {
+    title: 'newAwesomeTitle'
+  };
+
+  inventory.editItem(id, edits)
+    .then(() => res.send())
+    .catch(error => {
+      res.status(400).send(error.message); 
+    });
+});
+
+app.delete('/delete', (req, res) => {
+  // temporary id for testing
+  const id = 2;
+
+  inventory.deleteItem(id)
+    .then(() => res.send())
+    .catch(error => {
+      res.status(404).send(error.message);
     });
 });
 
@@ -32,7 +57,6 @@ app.get('/list', (req, res) => {
       res.status(502).send(error.message);
     });
 });
-
 
 // run server process
 const server = app.listen(process.env.PORT || 8000, () => {
