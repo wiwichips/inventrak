321dd2ff20596c7491176f850113da9ea64f18fb
Will Pringle <willkantorpringle+1@gmail.com>
2022-01-15 12:03:05 -0500

    Add placeholder endpoint for create

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,19 @@ const app = express();
 // serve html
 app.get('/', express.static(path.join(__dirname, '../public')));
 
+app.get('/create', (req, res) => {
+
+  const title = 'test';
+
+  inventory.createItem(title)
+    .then(id => {
+      res.send({id});
+    })
+    .catch(error => {
+      res.status(400).send(error.message);
+    });
+});
+
 app.get('/list', (req, res) => {
   inventory.listItems()
     .then(items => {
