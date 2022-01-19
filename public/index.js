cd6bebdde6805aa0ee2b5d7558401db14cdf95d7
Will Pringle <willkantorpringle+1@gmail.com>
2022-01-19 07:42:15 -0500

    Use values from frontend instead of hardcoded test values

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 const url = 'http://localhost:8000/';
 
-async function createItem() {
-  // hardcoding values
-  const body = { title: 'test title' };
+async function createItem(title, quantity) {
+  const body = { title, quantity };
+
+  console.log(body);
 
   const response = await fetch(`${url}create`, {
     method: 'POST',
@@ -10,18 +11,10 @@ async function createItem() {
     headers: { 'Content-Type': 'application/json' }
   });
   const result = await response.json();
-  console.log(result);
 }
 
-async function editItem() {
-  // hardcoding values
-  const proto = {
-    title: 'editedName'
-  }
-  const body = {
-    prototype: proto,
-    id: 2,
-  };
+async function editItem(id, prototype) {
+  const body = { id, prototype };
 
   const response = await fetch(`${url}edit`, {
     method: 'POST',
@@ -30,10 +23,7 @@ async function editItem() {
   });
 }
 
-async function remove() {
-  // hardcoding values
-  const id = 3;
-  
+async function remove(id) {
   const body = { id };
   const response = await fetch(`${url}delete`, {
     method: 'DELETE',
@@ -45,15 +35,46 @@ async function remove() {
 async function updateList() {
   const response = await fetch(`${url}list`);
   const list = await response.json();
-  let str = '';
-  for (property in list) {
-    str += `[${property}] ~ ${list[property].title}<br>`;
-  }
 
-  document.getElementById("list").innerHTML = JSON.stringify(str);
+  document.getElementById("list").innerHTML = JSON.stringify(list, null, '<br>');
 }
 
-createItem().then(updateList).then(remove).then();
 
-// set values
-document.getElementById("list").innerHTML = "My First JavaScript";
+// event listeners
+document.getElementById("create-button").addEventListener("click", () => {
+  const title = document.getElementById("create-title").value;
+  const quantityStr = document.getElementById("create-quantity").value;
+  const quantityInt = quantityStr === '' ? 0 : Number(quantityStr);
+
+  if (title === '')
+    throw new Error('Title field is empty');
+
+  createItem(title, quantityInt).then(updateList);
+});
+
+document.getElementById("edit-button").addEventListener("click", () => {
+  const id = Number(document.getElementById("edit-id").value);
+  const title = document.getElementById("edit-title").value;
+  const quantity = document.getElementById("edit-quantity").value;
+  const prototype = {};
+
+  if (!id)
+    throw new Error('ID required to edit an item');
+
+  if (title)
+    prototype.title = title;
+
+  if (quantity !== '')
+    prototype.quantity = Number(quantity);
+
+  editItem(id, prototype).then(updateList);
+});
+
+document.getElementById("delete-button").addEventListener("click", () => {
+  const id = Number(document.getElementById("delete-id").value);
+
+  remove(id).then(updateList);
+});
+
+updateList();
+
