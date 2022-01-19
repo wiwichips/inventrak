ffa139167ed47119cc88ba81e89f147721eaa3ba
Will Pringle <willkantorpringle+1@gmail.com>
2022-01-19 17:55:32 -0500

    Add an option to populate the database with a populate flag

--- setup.js
+++ setup.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
+
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const schemas = require('./backend/database/schemas');
+const orm = require('./backend/database/orm');
 
 function createDatabase(conn, name) {
   return conn.query(`CREATE DATABASE ${name};`);
@@ -13,7 +15,6 @@ async function createTable(conn, name, columns, pkey) {
   for (prop in columns)
     str += `${prop} ${columns[prop]},`;
   str += `PRIMARY KEY (${pkey}));`;
-  console.log(str);
   return conn.query(str);
 }
 
@@ -21,16 +22,18 @@ function clearDatabase(conn, name) {
   return conn.query(`DROP DATABASE ${name}`); 
 }
 
-async function main() {
+// clear database and create a new database and table
+async function setupDatabase() {
   const options = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
   };
 
+  // create database and clear it if it already exists
   const conn1 = await mysql.createConnection(options);
 
-  // attempt to clear all databases
+  console.log(`\tAttemping to delete the ${schemas.databaseName} database if it exists...`);
   try {
     await clearDatabase(conn1, schemas.databaseName);
   } catch (error) {
@@ -40,12 +43,13 @@ async function main() {
       console.error(error);
   } 
 
+  console.log(`\tCreating the ${schemas.databaseName} Database...`);
   await createDatabase(conn1, schemas.databaseName);
 
   await conn1.end();
 
+  // connect to the newly created database and add the tables
   options.database = process.env.DB_NAME;
-
   const conn2 = await mysql.createConnection(options);
 
   const tablePromises = [];
@@ -53,10 +57,46 @@ async function main() {
     let table = schemas.tables[index];
     tablePromises.push(createTable(conn2, table.name, table.columns, table.pkey));
   }
+
+  console.log('\tCreating tables...');
   await Promise.all(tablePromises);
+
+
+  console.log('Database is set up and ready to use!');
+
+  for (i in process.argv) {
+    if (process.argv[i].toLowerCase() === 'populate') {
+      console.log('\nPopulating the database with example values!');
+      await populateDatabase(conn2);
+    } 
+  }
 
   await conn2.end();
 }
 
-main();
+// populate the item table in the database with sample data
+function populateDatabase(conn) {
+  const adjectives = [ 'stinky', 'slimey', 'rotten', 'expired' ];
+  const nouns = [ 'apple', 'baguette', 'milk', 'chickpeas' ];
+  const rows = [];
+
+  for (i in nouns) {
+    let j = Math.floor(Math.random() * adjectives.length);
+    rows.push({
+      title: `${adjectives[j]} ${nouns[i]}`,
+      quantity: Math.floor(Math.random() * 1000),
+    });
+  }
+
+  console.log(rows);
+  const queryString = orm.insert(rows, schemas.databaseName, 'item');
+
+  
+  console.log(queryString);
+  return conn.query(queryString);
+}
+
+console.log('Attemping to setup the database!');
+setupDatabase().then(() => {
+});
 
